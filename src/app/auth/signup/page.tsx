6dfa95b9870d56b4569a33310b66d4449781ed65
Kleuber Silva <kleuber20@hotmail.com>
2024-03-27 13:33:17 -0300

    Add Supabase, React Query, and Tailwind CSS to Next.js app
    * Add `@supabase/ssr`, `@supabase/supabase-js`, and `@tanstack/react-query` packages
    * Create a `ReactQueryProvider` component to wrap the app
    * Update `page.tsx` and `layout.tsx` to use Supabase and React Query
    * Add Tailwind CSS and update global styles
    * Update `package.json` with new dependencies and version bumps

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,5 @@
+export default function SignUP() {
+    return (
+        <div>signup</div>
+    )
+}
---
+++ src/hooks/useSupabase.tsx
@@ -0,0 +1,9 @@
+import { getSupabaseBrowserClient } from '../utils/supabase';
+import { useMemo } from 'react';
+
+
+function useSupabase() {
+    return useMemo(getSupabaseBrowserClient, []);
+}
+
+export default useSupabase;
